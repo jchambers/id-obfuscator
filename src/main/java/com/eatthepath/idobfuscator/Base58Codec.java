6bccfe0f908714f44d438f1b3112511a6e1e3c45
Jon Chambers <jon.chambers@gmail.com>
2020-11-14 10:50:51 -0500

    Generalize the base58 codec.

--- src/main/java/com/eatthepath/idobfuscator/Base58Codec.java
+++ src/main/java/com/eatthepath/idobfuscator/Base58Codec.java
@@ -17,20 +17,82 @@ public class Base58Codec implements Codec {
 
     @Override
     public String encodeInteger(final int i) {
-        return encodeLong(i);
+        final byte[] intBytes = new byte[4];
+
+        intBytes[0] = (byte) ((i & 0xff000000) >> 24);
+        intBytes[1] = (byte) ((i & 0x00ff0000) >> 16);
+        intBytes[2] = (byte) ((i & 0x0000ff00) >> 8);
+        intBytes[3] = (byte)  (i & 0x000000ff);
+
+        return encode(intBytes);
     }
 
     @Override
     public String encodeLong(long l) {
-        final char[] encoded = new char[11];
-        int writeIndex = 0;
+        final byte[] longBytes = new byte[8];
 
-        while (l != 0) {
-            encoded[encoded.length - writeIndex++ - 1] = ALPHABET[(int) Long.remainderUnsigned(l, 58)];
-            l = Long.divideUnsigned(l, 58);
+        longBytes[0] = (byte) ((l & 0xff00000000000000L) >> 56);
+        longBytes[1] = (byte) ((l & 0x00ff000000000000L) >> 48);
+        longBytes[2] = (byte) ((l & 0x0000ff0000000000L) >> 40);
+        longBytes[3] = (byte) ((l & 0x000000ff00000000L) >> 32);
+        longBytes[4] = (byte) ((l & 0x00000000ff000000L) >> 24);
+        longBytes[5] = (byte) ((l & 0x0000000000ff0000L) >> 16);
+        longBytes[6] = (byte) ((l & 0x000000000000ff00L) >> 8);
+        longBytes[7] = (byte)  (l & 0x00000000000000ffL);
+
+        return encode(longBytes);
+    }
+
+    String encode(final byte[] bytes) {
+        // Adapted from https://github.com/bitcoin/libbase58/blob/master/base58.c
+        final StringBuilder stringBuilder = new StringBuilder();
+
+        int leadingZeroes = 0;
+
+        for (final byte b : bytes) {
+            if (b == 0) {
+                leadingZeroes++;
+            } else {
+                break;
+            }
         }
 
-        return new String(encoded, encoded.length - writeIndex, writeIndex);
+        final byte[] base58EncodedBytes = new byte[(((bytes.length - leadingZeroes) * 138) / 100) + 1];
+
+        int high = base58EncodedBytes.length - 1;
+
+        for (int i = leadingZeroes; i < bytes.length; i++) {
+            int carry = bytes[i] & 0xff;
+            int j;
+
+            for (j = base58EncodedBytes.length - 1; j > high || carry != 0; j--) {
+                carry += (base58EncodedBytes[j] & 0xff) * 256;
+                base58EncodedBytes[j] = (byte) (carry % 58);
+                carry /= 58;
+            }
+
+            high = j;
+        }
+
+        for (int i = 0; i < leadingZeroes; i++) {
+            stringBuilder.append(ALPHABET[0]);
+        }
+
+        int skip = 0;
+
+        for (final byte b : base58EncodedBytes) {
+            if (b == 0) {
+                skip++;
+            } else {
+                break;
+            }
+        }
+
+        for (int i = skip; i < base58EncodedBytes.length; i++) {
+            stringBuilder.append(ALPHABET[base58EncodedBytes[i]]);
+        }
+
+        return stringBuilder.toString();
     }
 
     @Override
@@ -47,6 +109,10 @@ public class Base58Codec implements Codec {
         long value = 0;
 
         for (final char c : string.toCharArray()) {
+            if (PLACE_VALUES[c] == -1) {
+                throw new IllegalArgumentException("Illegal base58 character: " + c);
+            }
+
             value *= 58;
             value += PLACE_VALUES[c];
         }
--- src/test/java/com/eatthepath/idobfuscator/Base58CodecTest.java
+++ src/test/java/com/eatthepath/idobfuscator/Base58CodecTest.java
@@ -2,17 +2,28 @@ package com.eatthepath.idobfuscator;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import java.nio.charset.StandardCharsets;
+
+import static org.junit.Assert.assertEquals;
 
 public class Base58CodecTest {
 
     @Test
     public void testEncodeLong() {
-        assertEquals("233QC4", new Base58Codec().encodeLong(0x0000287fb4cd));
+        assertEquals("1111233QC4", new Base58Codec().encodeLong(0x00000000287fb4cd));
     }
 
     @Test
     public void testDecodeLong() {
-        assertEquals(0x0000287fb4cd, new Base58Codec().decodeLong("111233QC4"));
+        assertEquals(0x00000000287fb4cd, new Base58Codec().decodeLong("11233QC4"));
+    }
+
+    @Test
+    public void testEncode() {
+        final Base58Codec codec = new Base58Codec();
+
+        assertEquals("2NEpo7TZRRrLZSi2U", codec.encode("Hello World!".getBytes(StandardCharsets.UTF_8)));
+        assertEquals("USm3fpXnKG5EUBx2ndxBDMPVciP5hGey2Jh4NDv6gmeo1LkMeiKrLJUUBk6Z", codec.encode("The quick brown fox jumps over the lazy dog.".getBytes(StandardCharsets.UTF_8)));
+        assertEquals("11233QC4", codec.encode(new byte[] { 0x00, 0x00, 0x28, 0x7f, (byte) 0xb4, (byte) 0xcd }));
     }
 }
